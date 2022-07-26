0319ad2e764c563c583b90387cf0fc7aac843f82
onkyoh <adnanradwan_8@hotmail.com>
2022-07-25 23:38:56 -0400

    put location fetcher in useeffect

--- src/components/GameScreen.tsx
+++ src/components/GameScreen.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { MapContainer, Marker, TileLayer} from 'react-leaflet'
-import Leaflet, { Handler } from "leaflet"
+import Leaflet from "leaflet"
 import SetCenter from './SetCenter'
 
 interface shadow {
@@ -9,7 +9,6 @@ interface shadow {
 }
 
 const GameScreen = () => {
-
   const [initialLocation, setInitialLocation] = useState<undefined | [number, number]>()
   const [updatedLocation, setUpdatedLocation] = useState<[number, number]>(initialLocation || [0, 0])
   const [previousLocation, setPreviousLocation] = useState<[number, number]>()
@@ -25,17 +24,20 @@ const GameScreen = () => {
     return shadowNumber
   }
 
-  const populateShadows = () => {
+  const populateShadows = (currentPos: number[] | undefined) => {
+    if (!currentPos) {
+      return
+    }
     let tempShadows: shadow[] = [...shadows]
 
-    let shadowDensity = amountOfShadows()
+    const shadowDensity = amountOfShadows()
 
     for (let i = 0; i < shadowDensity; i++) {
       const yTranslation = Math.random() * 0.0005 * (Math.round(Math.random()) * 2 - 1)
       const xTranslation = Math.random() * 0.0004 * (Math.round(Math.random()) * 2 - 1)
         tempShadows[i] = {
           id: Math.random() * 1000,
-          position: [updatedLocation[0] + yTranslation, updatedLocation[1] + xTranslation]
+          position: [currentPos[0] + yTranslation, currentPos[1] + xTranslation]
         }
     }
     setShadows([...tempShadows])
@@ -49,7 +51,8 @@ const GameScreen = () => {
       xChange = previousLocation[1] - updatedLocation[1]
       yChange = previousLocation[0] - updatedLocation[0]
       if ((xChange || yChange > 0.0003) || (xChange || yChange < -0.0003)) {
-        populateShadows()
+        populateShadows(updatedLocation)
+        console.log("shadows updated")
       }
     }
   }, [updatedLocation])
@@ -62,15 +65,16 @@ const GameScreen = () => {
     let  location: [number, number] = [position.coords.latitude, position.coords.longitude]
     setInitialLocation([...location])
     setUpdatedLocation([...location])
-    populateShadows()
-    
+    populateShadows(location)
 }
 
 const failure = (err: {message: string}) => {
   console.log(err.message)
 }
 
-navigator.geolocation.getCurrentPosition(success, failure)
+useEffect(() => {
+  navigator.geolocation.getCurrentPosition(success, failure)
+}, [])
 
 
 
@@ -78,7 +82,7 @@ navigator.geolocation.getCurrentPosition(success, failure)
     <>
     {initialLocation ?
       <>
-        <MapContainer className="map" center={initialLocation} zoom={19} dragging={false} zoomControl={false} doubleClickZoom={false} attributionControl={false}>
+        <MapContainer className="map" center={updatedLocation || initialLocation} zoom={19} dragging={false} zoomControl={false} doubleClickZoom={false} attributionControl={false} touchZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
                 url='https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
