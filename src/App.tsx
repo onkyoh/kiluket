097c164058d3a4ea231a90b775a88837a186b054
onkyoh <adnanradwan_8@hotmail.com>
2022-09-21 22:02:02 -0400

    button to request fullscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import './styles/App.css';
 import GameScreen from './components/GameScreen'
+import {useState} from 'react'
 
 
 function App() {
 
+  const [fullscreen, setFullscreen] = useState(false)
+
+  const goFullScreen = () => {
+    document.body.requestFullscreen()
+    setFullscreen(true)
+  }
+
   return (
     <div className='app'>
+      {!fullscreen ?
+      <button onClick={goFullScreen} id='fullscreen_button'>Click to allow fullscreen to continue</button>
+      :
       <GameScreen/>
+      }
     </div>
   );
 }
